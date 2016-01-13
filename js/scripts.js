d3cb16767e2e66bb5cbb3ff25b59d1ca95ae4743
Anna Becker and Taylor Pokoj <pokojt@gmail.com>
2016-01-13 15:49:43 -0800

    Loop flavors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(".flavors").submit(function(event) {
-  var flavors = ["first", "second", "third"]
-}
+$(document).ready(function() {
+  $(".flavors").submit(function(event) {
+    var flavors = ["first", "second", "third"]
+
+    flavors.forEach(function(flavor) {
+      var userInput = $("input#" + flavor).val();
+      $("." + flavor).text(userInput);
+    });
+    $(".answer").show();
+
+    event.preventDefault();
+  });
+});
